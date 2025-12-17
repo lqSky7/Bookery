1dd5822fd07ef255656c0b4b1418802da50d0102
ca5 <catinice@outlook.com>
2025-12-17 23:14:08 +0530

    Initial Commit
    Change-Id: I9823e78d64735d6dc95fedf1ce54128bf1279a73

---
+++ Bookery/BookeryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookeryApp.swift
+//  Bookery
+//
+//  Created by ca5 on 17/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BookeryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
